0cdfe7f8cf8634ee701bae3c35ed34e785bff8d7
CNEC <andre9562015@hotmail.com>
2018-06-04 21:36:14 -0300

    imagenssjaksasaksjalsasalk

---
+++ views/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!-- Estrutura Site  -->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Joseph">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Joseph Fotografias</title>
+        <meta name="description" content="Empresa de fotos">
+        <meta name="keywords" content="fotos, empresa, joseph, RS">
+        <link rel="stylesheet" type="text/css" href="../libraries/style.css">
+        <script src="../libraries/jquery.cycle.all.js" type="text/javascript"></script>
+        <script src="../libraries/jquery-3.3.1.min.js"></script>
+        <script src="../libraries/javascript.js"></script>
+    </head>
+
+    <body>
+        <header>
+            <?php require_once("../libraries/body.php"); ?>
+
+            <!-- Imagens do Slide -->
+            <div class="ind-slide"
+        </div>
+
+    </header>
+
+    <!-- Anúncio das fotos -->
+    <main>
+    </main>
+
+    <!--Rodapé-->
+    <footer>
+    </footer>
+
+</body>
+</html>
