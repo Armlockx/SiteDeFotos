d907d666370bb6ed28650b413b7694d8eec38b22
Bruna <brunarequiao@ads.cnecsan.edu.br>
2018-06-19 19:59:05 -0300

    login do google

--- libraries/javascript.js
+++ libraries/javascript.js
@@ -1,26 +1,42 @@
 /*Funções javascript*/
 
+function validacao() {
+    if(document.form.user.value == "") {
+        alert("Preencha o campo nome");
+        document.form.user.focus();
+        return false;
+    }
+
+    if(document.form.passw.value.length > 8) {
+        alert("A senha não pode conter mais que 8 carácteres");
+        document.form.passw.focus();
+        return false;
+    }
+
+    if(!document.form.passw.value) {
+        alert("Preencha o campo senha");
+        document.form.passw.focus();
+        return false;
+    }
+}
+
 $(document).ready(function() {
-    
+
     //ao clicar no indbotao irá mostrar o menu
     $('.indbotao').on('click', function() {
         $('nav li').toggle('indclose');
 
-    })
+    });
 
     /*nossa classe ind-slide vai chamar a função cycle*/
-    $(document).ready(function () {
-
-        $('.ind-slide').cycle({
-            fx: 'fade'
-        })
-
+    $('.ind-slide').cycle({
+        fx: 'fade'
     });
 
     /*ao clicar no nav li, pegaremos o atributo href do link e realizaremos o encaminhamento do usuário para aquela localização*/
-      $('nav li').on('click', function() {
-         var link = $(this).children('a').attr('href');
-         window.location.href = link;
-      });
-});
+    $('nav li').on('click', function() {
+        var link = $(this).children('a').attr('href');
+        window.location.href = link;
+    });
 
+});
